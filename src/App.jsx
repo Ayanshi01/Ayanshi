962ddda840eafce24913fd087b7a5ba76bd60190
Ayanshi01 <btech25193.24@bitmesra.ac.in>
2025-02-19 10:44:25 +0000

    ayanshi01

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ayanshi Bosu",
+  title: "Graphic Designer",
+  email: "ayanshibosu@gmail.com",
+  gitHub: "Ayanshi01",
+  instagram: "ayanshi_bosu",
+  linkedIn: "ayanshibosu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "AYANSHI",
+  youTube: "bose",
 };
 
 const primaryColor = "#4E567E";
